05c061dc1d725fdd3360dca529b8ad91f865d3ef
Tushar-Alec <petermoor822@gmail.com>
2024-02-12 16:12:38 +0530

    Add a `hasRead` property to the `Book` class and update the `info` method to include the read status.

--- library.js
+++ library.js
@@ -1,15 +1,16 @@
-function Book(title, author, pages) {
+function Book(title, author, pages , hasRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.hasRead = hasRead;
     this.info = function() {
-        console.log(this.title,"is written by", this.author,"and has",this.pages,"pages.");
+        const readStatus = this.hasRead ? "read" : "not read yet";
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
       };
 }
 
-const book1 = new Book('70 days', 'abc' , '240');
+const book1 = new Book('70 days', 'abc' , '240' , 'read');
 
-const book2 = new Book('The art of war', 'sun tzu' , '200');
+const book2 = new Book('The art of war', 'sun tzu' , '200' ,'');
 
-book1.info();
-book2.info();
+console.log(book2.info());
