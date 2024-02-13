59fcaf222fa172200a1793dc62aed877f01d80ce
Tushar-Alec <petermoor822@gmail.com>
2024-02-13 17:17:48 +0530

    Changed function and variable names to follow camelCase convention. Removed unnecessary semicolons. Fixed a typo in the `info` method. Added a check for valid input in the `addBookToLibrary` function.

--- library.js
+++ library.js
@@ -1,32 +1,30 @@
 const myLibrary = [];
 
-function Book(title, author, pages , hasRead) {
+function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
-    this.info = function() {
+    this.info = function () {
         const readStatus = this.hasRead ? "read" : "not read yet";
         return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-      };
+    };
 }
 
-
 function addBookToLibrary() {
-  const  title = prompt( 'Please enter the books title:');
-  const author = prompt('Enter the authors name:');
-  const pages = prompt('How many pages does this book have?');
-  const hasRead = prompt("Have you read the book? (yes/no)").toLowerCase();
+    const title = prompt('Please enter the book\'s title:');
+    const author = prompt('Enter the author\'s name:');
+    const pages = parseInt(prompt('How many pages does this book have?'), 10);
+    const hasRead = prompt("Have you read the book? (yes/no)").toLowerCase();
 
-const isValidReadStatus = hasRead === 'yes' || hasRead === 'no';
-    if (!isValidReadStatus) {
-        alert("Invalid input for 'hasRead' field. Please enter 'yes' or 'no'.");
+    if (!title || !author || isNaN(pages) || (hasRead !== 'yes' && hasRead !== 'no')) {
+        alert("Invalid input. Please make sure to provide valid values.");
         return;
     }
 
     const newBook = new Book(title, author, pages, hasRead === 'yes');
     myLibrary.push(newBook);
-  }
+}
 
 addBookToLibrary();
 console.log(myLibrary);
