05a3cb6c354f30d1d9c0a64d00471ab78984e8e5
Tushar-Alec <petermoor822@gmail.com>
2024-02-21 23:04:45 +0530

    Added a theme toggle feature to the library app.

--- library.js
+++ library.js
@@ -105,3 +105,12 @@ function getRandomColor() {
   return color;
 }
 
+function setTheme() {
+  const root = document.documentElement;
+  const newTheme = root.className === 'dark' ? 'light' : 'dark';
+  root.className = newTheme;
+  
+  document.querySelector('.theme-name').textContent = newTheme;
+}
+
+document.querySelector('.theme-toggle').addEventListener('click', setTheme)
